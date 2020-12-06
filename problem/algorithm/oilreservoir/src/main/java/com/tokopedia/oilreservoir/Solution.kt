b975daac94dcfb8cd5002dafbb50cb9746f270d6
liandaraaa <lianda.ramadhana31@gmail.com>
2020-12-06 15:49:39 +0700

    delete unused code

--- problem/algorithm/oilreservoir/src/main/java/com/tokopedia/oilreservoir/Solution.kt
+++ problem/algorithm/oilreservoir/src/main/java/com/tokopedia/oilreservoir/Solution.kt
@@ -13,19 +13,12 @@ object Solution {
 
         for(i in height.indices){
             val currentHeight = height[i]
-            println("oil current$previousHeight")
-            println("oil next $currentHeight")
-
             if (i !=  height.size - 1){
                 val nextHeight = height[i+1]
-                println("oil after next $nextHeight")
-
                 if ((previousHeight > currentHeight) && (nextHeight > currentHeight) ){
                     oil += (nextHeight - currentHeight)
                 }
-
                 previousHeight = max(currentHeight,previousHeight)
-                println("oil $oil")
             }
         }
 
